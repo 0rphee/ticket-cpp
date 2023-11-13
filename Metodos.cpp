b022ff80f2a241d56105511d24b6dbae048947da
Carlos Flores <117421002+CarlosFloresVaras@users.noreply.github.com>
2023-11-12 18:02:39 -0600

    Header y Metodos
    Hice un inicio tanto en las clases y definiciones compañeros

---
+++ Metodos.cpp
@@ -0,0 +1,124 @@
+// Metodos.cpp
+
+#include "Header.h"
+
+Seccion::Seccion() {
+    nombreSeccion = "";
+    cantidadBoletosDisponibles = 0;
+    precioBoleto = 0.0;
+    listaBoletosVendidos = nullptr;
+}
+
+Seccion::~Seccion() {
+    // Agregar lógica de liberación de memoria si es necesario
+}
+
+void Seccion::InicializarSeccion(string nombre, int cantidadBoletos, double precioBoleto) {
+    nombreSeccion = nombre;
+    cantidadBoletosDisponibles = cantidadBoletos;
+    this->precioBoleto = precioBoleto;
+}
+
+void Seccion::ComprarBoletos(int cantidad, string nombreComprador, int numTarjeta) {
+    if (cantidad > 0 && cantidad <= cantidadBoletosDisponibles) {
+        Nodo* nuevo = new Nodo();
+        nuevo->nombreComprador = nombreComprador;
+        nuevo->numTarjeta = numTarjeta;
+        nuevo->cantidadBoletos = cantidad;
+        nuevo->totalCompra = cantidad * precioBoleto;
+
+        nuevo->sig = listaBoletosVendidos;
+        listaBoletosVendidos = nuevo;
+
+        // Actualizar la cantidad de boletos disponibles
+        cantidadBoletosDisponibles -= cantidad;
+
+        cout << "\n\tCompra realizada con éxito.\n";
+        cout << "\tTotal a pagar: $" << nuevo->totalCompra << "\n";
+    } else {
+        cout << "\n\tNo hay suficientes boletos disponibles.\n";
+    }
+}
+
+void Seccion::ConsultarBoletos() {
+    cout << "\n\tSección: " << nombreSeccion << "\n";
+    cout << "\tBoletos disponibles: " << cantidadBoletosDisponibles << "\n";
+    cout << "\tPrecio por boleto: $" << precioBoleto << "\n";
+}
+
+Nodo* Seccion::ObtenerListaBoletosVendidos() {
+    return listaBoletosVendidos;
+}
+
+Evento::Evento() {
+    comprasFinalizadas = false;
+}
+
+Evento::~Evento() {
+    // Agregar lógica de liberación de memoria si es necesario
+}
+
+void Evento::MostrarVentaBoletos() {
+    seccionGA.ConsultarBoletos();
+    seccionGB.ConsultarBoletos();
+    seccionVIP.ConsultarBoletos();
+}
+
+void Evento::BuscarPorComprador(string nombre) {
+    cout << "\nBuscando boletos comprados por: " << nombre << "\n";
+
+    // Obtener las listas de boletos vendidos de cada sección
+    Nodo* actualGA = seccionGA.ObtenerListaBoletosVendidos();
+    Nodo* actualGB = seccionGB.ObtenerListaBoletosVendidos();
+    Nodo* actualVIP = seccionVIP.ObtenerListaBoletosVendidos();
+
+    // Buscar por comprador en cada sección
+    bool encontrado = false;
+
+    // Sección General A
+    while (actualGA != nullptr) {
+        if (actualGA->nombreComprador == nombre) {
+            cout << "Boleto encontrado en General A - Número de Tarjeta: " << actualGA->numTarjeta << "\n";
+            encontrado = true;
+        }
+        actualGA = actualGA->sig;
+    }
+
+    // Repetir para sección General B y VIP...
+
+    if (!encontrado) {
+        cout << "No se encontraron boletos comprados por: " << nombre << "\n";
+    }
+}
+
+void Evento::BuscarPorSeccion(string seccion) {
+    cout << "\nBuscando boletos en la sección: " << seccion << "\n";
+
+    // Determinar la sección y obtener la lista de boletos vendidos
+    Seccion* seccionActual = nullptr;
+    Nodo* listaBoletosVendidos = nullptr;
+
+    if (seccion == "General A") {
+        seccionActual = &seccionGA;
+        listaBoletosVendidos = seccionActual->ObtenerListaBoletosVendidos();
+    } else if (seccion == "General B") {
+        seccionActual = &seccionGB;
+        listaBoletosVendidos = seccionActual->ObtenerListaBoletosVendidos();
+    } else if (seccion == "VIP") {
+        seccionActual = &seccionVIP;
+        listaBoletosVendidos = seccionActual->ObtenerListaBoletosVendidos();
+    } else {
+        cout << "\nSección no válida.\n";
+        return;
+    }
+
+    // Mostrar la lista de boletos vendidos en la sección especificada
+    while (listaBoletosVendidos != nullptr) {
+        cout << "Número de Tarjeta: " << listaBoletosVendidos->numTarjeta
+             << "\tComprador: " << listaBoletosVendidos->nombreComprador
+             << "\tCantidad de Boletos: " << listaBoletosVendidos->cantidadBoletos
+             << "\tTotal de Compra: $" << listaBoletosVendidos->totalCompra << "\n";
+
+        listaBoletosVendidos = listaBoletosVendidos->sig;
+    }
+}
