eb267b47fd81b42719bcd2ffee0230457ea1c6c2
0rphee <0rph3e@proton.me>
2023-11-12 23:10:10 -0600

    añadir comentarios y reescribir primeros prompts

--- Main.cpp
+++ Main.cpp
@@ -45,49 +45,55 @@ int main()
             cout << endl;
             cout << "Menu Principal" << endl;
             cout << "1. Comprar Boletos" << endl;
-            cout << "2. Consultar Boletos" << endl;
+            cout << "2. Consultar información de Boletos" << endl;
             cout << "3. Finalizar Compras" << endl;
-            cout << "4. Salir" << endl;
             cout << "Ingrese una opcion: ";
             cin >> opt;
     
             switch (opt) {
             case 1:
-                evento.MostrarVentaBoletos();
+                cout << "** Sin implementar **" << endl;
                 break;
             case 2:
-                cout << endl;
-                cout << "1. Consultar por Comprador" << endl;
-                cout << "2. Consultar por Seccion" << endl;
-                cout << "Ingrese una opcion: ";
-                cin >> opt;
-    
-                switch (opt) {
-                case 1:
-                    cout << "Ingrese el nombre del comprador: ";
-                    cin >> nombre;
-                    evento.BuscarPorComprador(nombre);
-                    break;
-                case 2:
-                    cout << "Ingrese el nombre de la seccion: ";
-                    cin >> seccion;
-                    evento.BuscarPorSeccion(seccion);
-                    break;
-                default:
-                    cout << "Opcion invalida" << endl;
-                    break;
-                }
-                break;
-            case 3:
-                cout << "Compras finalizadas, resumen de la venta de boletos: " << endl;
                 evento.MostrarVentaBoletos();
                 break;
-            case 4:
-                cout << "Gracias por usar el sistema de venta de boletos" << endl;
+            case 3:
+                cout << "\nCompras finalizadas, resumen de la venta de boletos" << endl;
+                do {
+                    cout << "\n\n\t1. Mostrar lista de Ventas" << endl;
+                    cout << "\t2. Consultar por Comprador" << endl;
+                    cout << "\t3. Consultar por Seccion" << endl;
+                    cout << "\t4. Salir de la aplicacion" << endl;
+                    cout << "\tIngrese una opcion: ";
+                    cin >> opt;
+
+                    switch (opt) {
+                    case 1:
+                        evento.MostrarVentaBoletos();
+                        break;
+                    case 2:
+                        cout << "Ingrese el nombre del comprador: ";
+                        cin >> nombre;
+                        evento.BuscarPorComprador(nombre);
+                        break;
+                    case 3:
+                        cout << "Ingrese el nombre de la seccion: ";
+                        cin >> seccion;
+                        evento.BuscarPorSeccion(seccion);
+                        break;
+                    case 4:
+                        cout << "Gracias por usar el sistema de venta de boletos" << endl;
+                        break;
+                    default:
+                        cout << "** OPCION INVALIDA" << endl;
+                        break;
+                    }
+                } while (opt != 4);
+                opt = 3; // TERMINA
                 break;
             default:
-                cout << "Opcion invalida" << endl;
+                cout << "** OPCION INVALIDA **" << endl;
                 break;
             }
-        } while (opt != 4);
+        } while (opt != 3);
  }
